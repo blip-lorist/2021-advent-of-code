a0aa4f0522ed0f2e0f4bb9a18e7b1d2119839ef4
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-02 09:20:15 -0800

    Add depth attr, movement down

--- 02/submarine.py
+++ 02/submarine.py
@@ -1,9 +1,11 @@
 class Submarine:
     def __init__(self):
-        self.coordinates = {"horizontal": 0 }
+        self.coordinates = {"horizontal": 0, "depth": 0}
 
 
     def move(self, direction, distance):
         if direction == "forward":
             self.coordinates["horizontal"] += distance
 
+        if direction == "down":
+            self.coordinates["depth"] += distance
--- 02/test/test_submarine_coordinates.py
+++ 02/test/test_submarine_coordinates.py
@@ -10,11 +10,19 @@ class TestSubmarineClass(unittest.TestCase):
         new_submarine = Submarine();
         self.assertTrue(hasattr(new_submarine, "coordinates"))
         self.assertTrue("horizontal" in new_submarine.coordinates)
+        self.assertTrue("depth" in new_submarine.coordinates)
 
-    def test_move(self):
+    def test_move_forward(self):
         new_submarine = Submarine();
         new_submarine.move("forward", 5)
-        expected_coordinates = {"horizontal": 5}
+        expected_coordinates = {"horizontal": 5, "depth": 0}
+        actual_coordinates = new_submarine.coordinates
+        self.assertEqual(expected_coordinates, actual_coordinates)
+
+    def test_move_down(self):
+        new_submarine = Submarine();
+        new_submarine.move("down", 5)
+        expected_coordinates = {"horizontal": 0, "depth": 5}
         actual_coordinates = new_submarine.coordinates
         self.assertEqual(expected_coordinates, actual_coordinates)
 
