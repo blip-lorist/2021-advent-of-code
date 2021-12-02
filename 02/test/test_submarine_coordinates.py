981e29e7cc97a6b693b1ccebb03961a874ead8ef
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-02 09:12:23 -0800

    Update test name

--- 02/test/test_submarine_coordinates.py
+++ 02/test/test_submarine_coordinates.py
@@ -1,7 +1,7 @@
 import unittest
 from submarine import Submarine
 
-class TestPartOne(unittest.TestCase):
+class TestSubmarineClass(unittest.TestCase):
     def test_init_submarine(self):
         new_submarine = Submarine();
         self.assertIsInstance(new_submarine, Submarine)
