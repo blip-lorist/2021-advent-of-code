29f69b995257ef819117261ec103f1e534677afd
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-02 09:11:17 -0800

    Test for attributes

--- 02/test/test_submarine_coordinates.py
+++ 02/test/test_submarine_coordinates.py
@@ -5,3 +5,9 @@ class TestPartOne(unittest.TestCase):
     def test_init_submarine(self):
         new_submarine = Submarine();
         self.assertIsInstance(new_submarine, Submarine)
+
+    def test_submarine_coordinates(self):
+        new_submarine = Submarine();
+        self.assertTrue(hasattr(new_submarine, "coordinates"))
+
+
