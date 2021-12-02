66d26ae5559908422907bd076893801d103965e3
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-01 20:43:31 -0800

    Window sums

--- 01/depth_increases.py
+++ 01/depth_increases.py
@@ -6,3 +6,12 @@ def count_increases(depths):
 
     return increases
 
+
+def window_sums_list(depths, window_size=3):
+    window_sums = []
+    for i, depth in enumerate(depths):
+        if i != 0 and i != (len(depths) - 1):
+            window_sum = depths[i-1] + depth + depths[i+1]
+            window_sums.append(window_sum)
+
+    return window_sums
--- 01/test/test_depth_increases.py
+++ 01/test/test_depth_increases.py
@@ -1,5 +1,5 @@
 import unittest
-from depth_increases import count_increases
+from depth_increases import count_increases, window_sums_list
 
 class TestFuelNeeded(unittest.TestCase):
     def test_count_increases(self):
@@ -8,4 +8,13 @@ class TestFuelNeeded(unittest.TestCase):
 
         actual_count = count_increases(test_input)
         self.assertEqual(expected_count, actual_count)
+
+
+    def test_window_sums(self):
+        test_input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        expected_sums = [607, 618, 618, 617, 647, 716, 769, 792]
+
+        actual_sums = window_sums_list(test_input)
+
+        self.assertEqual(expected_sums, actual_sums)
 
