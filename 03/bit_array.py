3be63da07296c511a2eaaeb04892fa7fe4db6f8b
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-03 01:49:18 -0800

    Decimal and binary gammas

--- 03/bit_array.py
+++ 03/bit_array.py
@@ -4,16 +4,22 @@ class BitArray:
     def __init__(self, bits):
         self.bits = np.array(bits)
         self.decimal_gamma = None
+        self.binary_gamma = None
 
     def compute_gamma_rate(self):
         binary_gamma_str = ""
+        binary_gamma_array = []
+
         rotated_array = np.rot90(self.bits, axes=(1,0))
+
         for bit_slice in rotated_array:
             most_frequent_bit = np.bincount(bit_slice).argmax()
+            binary_gamma_array.append(most_frequent_bit)
             binary_gamma_str += str(most_frequent_bit)
 
         decimal_gamma = int(binary_gamma_str, 2)
-        self.gamma_rate = decimal_gamma
+        self.decimal_gamma = decimal_gamma
+        self.binary_gamma = binary_gamma_array
 
 
 
--- 03/test/test_gamma_epsilon_product.py
+++ 03/test/test_gamma_epsilon_product.py
@@ -24,7 +24,11 @@ class TestComputeGammaRate(unittest.TestCase):
 
         new_bit_array = BitArray(bits)
 
-        expected_gamma_rate = 22
+        expected_decimal_gamma = 22
+        expected_binary_gamma = [1, 0, 1, 1, 0]
+
         new_bit_array.compute_gamma_rate()
-        actual_gamma_rate = new_bit_array.gamma_rate
-        self.assertEqual(expected_gamma_rate, actual_gamma_rate)
+        actual_decimal_gamma = new_bit_array.decimal_gamma
+        actual_binary_gamma = new_bit_array.binary_gamma
+        self.assertEqual(expected_decimal_gamma, actual_decimal_gamma)
+        self.assertEqual(expected_binary_gamma, actual_binary_gamma)
