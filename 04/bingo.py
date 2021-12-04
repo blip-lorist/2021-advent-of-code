b9b624ef8c6222333aa401f6c1d05f90f9868593
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-04 00:37:09 -0800

    Formatting

--- 04/bingo.py
+++ 04/bingo.py
@@ -1,3 +1,10 @@
+import numpy as np
+
 class BingoBoard:
     def __init__(self, values):
         self.values = values
+        self.marked_coordinates = []
+
+    def mark_tile(value):
+        return None
+
--- 04/test/test_bingo.py
+++ 04/test/test_bingo.py
@@ -3,8 +3,9 @@ from bingo import BingoBoard
 
 
 class TestBitArrayClass(unittest.TestCase):
-    def test_init_bingo_board(self):
-        board_values = [
+
+    def setUp(self):
+        self.board_values = [
             [14, 21, 17, 24, 4],
             [10, 16, 15, 9, 19],
             [18, 8, 23, 26, 20],
@@ -12,7 +13,8 @@ class TestBitArrayClass(unittest.TestCase):
             [2,  0,  12, 3,  7],
         ]
 
-        board = BingoBoard(board_values)
+    def test_init_bingo_board(self):
+        board = BingoBoard(self.board_values)
         self.assertTrue(hasattr(board, "values"))
-        self.assertEqual(board_values, board.values)
+        self.assertEqual(self.board_values, board.values)
 
