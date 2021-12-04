2aaed5c8b9b924c64cbb4c9ef39c390d70429803
Loraine Kanervisto <loraine@lorainekv.com>
2021-12-04 01:04:43 -0800

    Mark no win

--- 04/bingo.py
+++ 04/bingo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 
 class BingoBoard:
     def __init__(self, values):
@@ -8,6 +9,28 @@ class BingoBoard:
     def mark_tile(self, value):
         values_ndarray = np.array(self.values)
         coordinates = zip(*np.where(values_ndarray == value))
-        print(coordinates)
         self.marked_coordinates += coordinates
 
+    def has_win(self):
+        # Win if any x coordinate has a count of 5
+        # or if any y coordinate has a count of 5
+        counter = {
+            "x": Counter(),
+            "y": Counter(),
+            }
+
+        for coordinate in self.marked_coordinates:
+            x = coordinate[0]
+            y = coordinate[1]
+            counter["x"][x] += 1
+            counter["y"][y] += 1
+
+        for coordinate, count in counter["x"].items():
+            if count == 5:
+                return True
+
+        for coordinate, count in counter["y"].items():
+            if count == 5:
+                return True
+
+        return False
